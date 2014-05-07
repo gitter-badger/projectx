ead87ea9c3304273b103c2142767199ccaf50409
Chris Wenner <crwenner@gmail.com>
2014-05-07 12:11:13 -0400

    dded Persons table and file

---
+++ app/database/migrations/2014_05_07_145742_create_persons_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('persons', function(Blueprint $table)
+		{
+			// Primary Key
+			$table->increments('id');
+
+			// Fields
+			$table->string('f_name');
+			$table->string('m_initial');
+			$table->string('l_name');
+			$table->string('ssn')->nullable();
+			$table->date('dob')->nullable();
+			//Foreign Key to Person_type Table
+			$table->integer('person_type_id');
+
+			//timestamp
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Drop table
+		Schema::drop('persons');
+	}
+
+}
