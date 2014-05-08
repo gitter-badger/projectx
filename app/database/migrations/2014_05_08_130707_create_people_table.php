12f5050ea993205bb91eea7042e59668ee8ddcb5
Mark Myers <marcusmyers@gmail.com>
2014-05-08 09:10:53 -0400

    Fixed all table issues.  You will need to delete everything in testing.sqlite file and do a migrate install

---
+++ app/database/migrations/2014_05_08_130707_create_people_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration {
+
+	/**
+	* Run the migrations.
+	*
+	* @return void
+	*/
+	public function up()
+	{
+		//Create Persons Table
+		Schema::create('people', function(Blueprint $table)
+		{
+			// Primary Key
+			$table->increments('id');
+
+			// Fields
+			$table->string('f_name');
+			$table->string('m_initial');
+			$table->string('l_name');
+			$table->string('ssn')->nullable();
+			$table->date('dob')->nullable();
+			//Foreign Key to Person_type Table
+			$table->integer('person_type_id');
+
+			//timestamp
+			$table->timestamps();
+		});
+	}
+
+	/**
+	* Reverse the migrations.
+	*
+	* @return void
+	*/
+	public function down()
+	{
+		// Drop table
+		Schema::drop('people');
+	}
+
+}
